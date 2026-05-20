519d049eb1558de804f74e6f60f40febd0e827c0
StephenNg25 <stephen.n180325@gmail.com>
2026-05-20 16:50:01 -0400

    Set up root route and query client

--- weather-forcast/src/router.tsx
+++ weather-forcast/src/router.tsx
@@ -1,19 +1,22 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-
-export function getRouter() {
-  const router = createTanStackRouter({
-    routeTree,
-    scrollRestoration: true,
-    defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
-  })
-
-  return router
-}
-
 declare module '@tanstack/react-router' {
   interface Register {
     router: ReturnType<typeof getRouter>
   }
 }
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+export const getRouter = () => {
+  const queryClient = new QueryClient();
+
+  const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+
+  return router;
+};
+
